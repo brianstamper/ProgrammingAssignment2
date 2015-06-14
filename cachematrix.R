86dc54e8fab27a49687ca445d48f2b440847c321
Brian Stamper <stamper.10@osu.edu>
2015-06-14 02:29:12 +0000

    Completed commentary describing the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions provide a matrix with
+## a cached calculation of the inverse.
+ 
+## Usage example, with a random matrix:
 
-## Write a short comment describing this function
+## First, let's create a random matrix, look at it,
+## and then look at its inverse the traditional way
+## using the solve() function.
+
+# > mat <- matrix(rnorm(9), 3, 3)
+# > mat
+# [,1]       [,2]       [,3]
+# [1,] -1.0853849  0.8721036  0.6051487
+# [2,]  0.9996967 -1.5262710  0.9950324
+# [3,]  0.1664028 -0.3034187 -1.2819075
+# > solve(mat)
+# [,1]       [,2]       [,3]
+# [1,] -1.85250169 -0.7663982 -1.4693961
+# [2,] -1.18698493 -1.0586724 -1.3820932
+# [3,]  0.04048029  0.1510953 -0.6436959
+ 
+## Now, we form a matrix with caching, and then show how
+## to look at it and its inverse.
+# > cmat <- makeCacheMatrix(mat)
+# > cmat$get()
+# [,1]       [,2]       [,3]
+# [1,] -1.0853849  0.8721036  0.6051487
+# [2,]  0.9996967 -1.5262710  0.9950324
+# [3,]  0.1664028 -0.3034187 -1.2819075
+# > cacheSolve(cmat)
+# [,1]       [,2]       [,3]
+# [1,] -1.85250169 -0.7663982 -1.4693961
+# [2,] -1.18698493 -1.0586724 -1.3820932
+# [3,]  0.04048029  0.1510953 -0.6436959
+
+## And we see that if we request the inverse again
+## it uses the cached values.
+# > cacheSolve(cmat)
+# getting cached data
+# [,1]       [,2]       [,3]
+# [1,] -1.85250169 -0.7663982 -1.4693961
+# [2,] -1.18698493 -1.0586724 -1.3820932
+# [3,]  0.04048029  0.1510953 -0.6436959
+ 
+ 
+## makeCacheMatrix is a function which takes an ordinary
+## matrix as its input and returns a list object which
+## contains functions for getting and setting the matrix
+## as well as the matrix inverse.
+
+## cmat <- makeCacheMatrix(mat) constructs cmat using matrix mat
+## cmat$set(y)       sets the matrix to equal y
+## cmat$get()        returns the matrix
+## cmat$setsolve(s)  sets the inverse of the matrix to equal s
+## cmat$getsolve()   returns the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -18,10 +69,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that takes a makeCacheMatrix list
+## and returns the inverse of the matrix it represents. If
+## the inverse has not been calculated already, it calculates
+## it and stores the result. If the inverse had already been
+## calculated, it returns the cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   m <- x$getsolve()
   if(!is.null(m)) {
     message("getting cached data")
